1602a373f145ed818075b0161a5516dbc334d1c3
Emman Mlmb <emmanjibi@gmail.com>
2025-06-03 17:00:20 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,18 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import mainRoutes from './routes/main.js';
+const express = require('express');
+const path = require('path');
+const mainRoutes = require('./routes/main');
 
 const app = express();
 
-// Résolution du chemin absolu
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Moteur de template
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Routes
 app.use('/', mainRoutes);
 
-// Serveur
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`✅ SignSafe est en ligne sur http://localhost:${PORT}`);
+    console.log(`✅ SignSafe est en ligne sur http://localhost:${PORT}`);
 });
